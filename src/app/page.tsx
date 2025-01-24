7ff4ee6bc34d2cfe951f713eb5d5982a058463e3
masud-rana9 <rana61960@gmail.com>
2025-01-24 23:22:44 +0600

    update hero in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
+
 import Navbar from "@/components/Navbar";
 import Services from "@/components/Services";
 import Work from "@/components/Work";
